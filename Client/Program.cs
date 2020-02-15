46a156efcbe938245601b8b18fb8baf021cee299
dominik pytlewski <dominik@pytlewski.eu>
2020-02-15 11:18:04 +0100

    initial

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+
+namespace blazorfun.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            //builder.RootComponents.Add<App>("app");
+            
+            var host = builder.Build();
+
+            await Task.Run(async () =>
+            {
+                var js = host.Services.GetRequiredService<IJSRuntime>();
+                await js.InvokeVoidAsync("console.log", new object[] {"!@#!@#!@#!@#"});
+            });
+
+            await host.RunAsync();
+        }
+    }
+}
