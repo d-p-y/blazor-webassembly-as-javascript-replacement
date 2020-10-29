67295931c7c38b82bf30ae40e4831b9fc3797d4d
Dominik Pytlewski <dominik@pytlewski.eu>
2020-10-29 12:46:39 +0100

    updated to use latest blazor webassembly version

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿using System;
+using System;
+using System.Net.Http;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Text;
-using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.JSInterop;
 
 /* see README.md for reasoning behind this code */
-namespace blazorfun.Client {
+namespace blazorfun {
     public static class StringExtensions {
         public static string EscapeJsStringForSingleQuote(this string self) => self.Replace("'", "\\'");
     }
@@ -18,21 +20,22 @@ namespace blazorfun.Client {
         public string Id { get; private set; }
 
         public string tagName => Document.Js.Invoke<string>("evalDebug", $"window.getElementRefById('{Id}').tagName");
-        public string textContent { 
+        public string textContent {
             get => Document.Js.Invoke<string>("evalDebug", $"window.getElementRefById('{Id}').textContent");
-            set => Document.Js.Invoke<string>("evalDebug", $"window.getElementRefById('{Id}').textContent = '{value.EscapeJsStringForSingleQuote()}'"); }
+            set => Document.Js.Invoke<string>("evalDebug", $"window.getElementRefById('{Id}').textContent = '{value.EscapeJsStringForSingleQuote()}'");
+        }
 
-        public static HtmlElement FromId(string id) => new HtmlElement {Id = id};
+        public static HtmlElement FromId(string id) => new HtmlElement { Id = id };
 
         /// <summary>returns callbackId needed for removeEventListener</summary>
-        public int addEventListener(string eventName, Action<object> callback, bool useCapture=false) {
+        public int addEventListener(string eventName, Action<object> callback, bool useCapture = false) {
             var cbId = Document.callbackId++;
             Document._callbacks.Add(cbId, callback);
 
             Document.Js.Invoke<string>("evalDebug", $@"window.getElementRefById('{Id}')
                 .addEventListener(
                     '{eventName}', function (x) {{
-                    DotNet.invokeMethodAsync('{nameof(blazorfun)+"."+nameof(Client)}', '{nameof(Program.JsCallback)}', '{cbId}');
+                    DotNet.invokeMethodAsync('{nameof(blazorfun)}', '{nameof(Program.JsCallback)}', '{cbId}');
                 }}, 
                 {(useCapture ? "true" : "false")} 
             )");
@@ -48,15 +51,15 @@ namespace blazorfun.Client {
             Document.Js.InvokeVoid("evalDebug", $"window.getElementRefById('{Id}').appendChild(window.getElementRefById('{el.Id}'))");
         }
     }
-    
+
     public static class Document {
         public static IJSInProcessRuntime Js;
         public static int callbackId = 1;
-        
+
         public static IDictionary<int, Action<object>> _callbacks = new Dictionary<int, Action<object>>();
-        
+
         public static HtmlElement body => HtmlElement.FromId(Js.Invoke<string>("evalDebug", "document.body.id"));
-        
+
         public static HtmlElement createElement(string name) {
             var nameStr = "'" + name.EscapeJsStringForSingleQuote() + "'";
 
@@ -77,7 +80,7 @@ namespace blazorfun.Client {
 
     public class Program {
         private static IJSInProcessRuntime _js;
-        
+
         public static void Log(string msg) => _js.InvokeVoid("console.log", new object[] { msg });
 
         [JSInvokable]
@@ -95,7 +98,7 @@ namespace blazorfun.Client {
         private static int _onClickedCallbackId;
 
         public static async Task Main(string[] args) {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);            
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
             var host = builder.Build();
 
             await Task.Run(async () => {
@@ -103,7 +106,7 @@ namespace blazorfun.Client {
                 Document.Js = _js;
 
                 Log("!@#!@#!@#!@#");
-         
+
                 Log($"body has id={Document.body.Id} and tagname is {Document.body.tagName}");
 
                 Window.alert("hi there");
